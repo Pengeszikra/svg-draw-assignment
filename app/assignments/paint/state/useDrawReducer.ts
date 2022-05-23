e1053e17d5240c01a5eb45e413ad3ddc393c4ae2
Peter Vivo <peter.vivo@appercell.com>
2022-05-23 06:59:51 +0200

    redux saga added

--- app/assignments/paint/state/useDrawReducer.ts
+++ app/assignments/paint/state/useDrawReducer.ts
@@ -1,6 +1,12 @@
-import { useTroll } from 'react-troll';
+import { getDispatchedActions } from 'react-troll';
+import useSagaReducer from 'use-saga-reducer';
 import { TrollFactory } from '../../../utils/react-troll-declaration';
+import { mainSaga } from '../saga/mainSaga';
 import { drawActionsSet, drawInitialState, drawReducer } from './draw-control';
 import { IDrawState } from './paintState';
 
-export const useDrawReducer:TrollFactory<IDrawState> = () => useTroll(drawReducer, drawInitialState, drawActionsSet);
+export const useDrawReducer:TrollFactory<IDrawState> = () => {
+  const [state, dispatch] = useSagaReducer(mainSaga, drawReducer, drawInitialState);
+  const army = getDispatchedActions(drawActionsSet(), dispatch);
+  return [state, army];
+};
